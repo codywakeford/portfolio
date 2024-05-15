834df7b32ac080cefce37b2cdd8238f6003a440e
cwake3218 <cody@gourmai.co.uk>
2024-05-15 19:34:49 +0100

    c

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,14 +22,25 @@
             </div>
 
             <div class="nav-right-content">
-                <button><i class="fa-solid fa-bars"></i></button>
+                <button @click="toggleNav">
+                    
+                    <i v-if="showMobileNav"  class="fa-solid fa-x"></i>
+                    <i v-else class="fa-solid fa-bars"></i>
+                </button>
             </div>
+
+            
+            <div class="nav-sidebar" id="sideBar">
+                <a href="#home" @click="toggleNav">Home</a>
+                <a href="#about" @click="toggleNav">About Me</a>
+                <a href="#projects" @click="toggleNav">Projects</a>
+            </div>
+            
         </nav>
     </div>
 </template>
 
 <style scoped>
-
 nav img {
     width: 50px;
 }
@@ -39,38 +50,34 @@ nav img:hover {
 nav {
     display: flex;
     justify-content: space-between;
-
     position: fixed;
     top: 0;
     left: 0;
     right: 0;
     z-index: 4;
-
     width: 100%;
     max-width: 1800px;
     padding: 50px;
     margin-inline: auto;
-
     color: var(--text-primary);
     transition: background-color 0.5s;
-    
 }
 
 .nav-left-content, .nav-right-content {
     display: flex;
     align-items: center;
     gap: 20px;
-
     font-size: 1.5rem;
-    color:var(--text-primary);
+    color: var(--text-primary);
 }
 .nav-content {
     display: flex;
     justify-content: space-between;
     width: 1200px;
 }
+
 nav a {
-    font-size: 1.25rem;;
+    font-size: 1.25rem;
     text-decoration: none;
     color: var(--text-primary);
     transition: text-shadow 0.3s;
@@ -97,7 +104,6 @@ nav a:hover {
     50% { transform: translateY(-10px); }
     100% { transform: translateY(0); }
 }
-
 @keyframes flip {
     0% { rotate: 0deg; }
     100% { rotate: 720deg; }
@@ -106,8 +112,28 @@ nav a:hover {
 @media (max-width: 1600px) {
     .dark-background {
         background-color: var(--background-primary);
-        
     }
+}
+.nav-sidebar {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    gap: 10px;
+
+    padding: 50px 0;
+
+    position: fixed;
+    left: -100%;
+    top: 0;
+    bottom: 0;
+    min-width: 320px;
+    background-color: rgba(0,0,0,0.5);
+    backdrop-filter: blur(50px);
+    transition: left 0.3s ease;
+}
+
+.nav-sidebar a{
+    font-size: 3rem !important;
 }
 
 #mobileNav {
@@ -115,15 +141,12 @@ nav a:hover {
     justify-content: space-between;
     display: none;
 }
-
 #mobileNav button {
-    
     background-color: rgba(0,0,0,0);
     color: white;
     border: none;
     font-size: 1.5rem;
 }
-
 #mobileNav button:active {
     transform: translate(2px, 2px);
 }
@@ -131,20 +154,27 @@ nav a:hover {
 @media (max-width: 680px) {
     #nav {
         display: none;
-
     }
-
     #mobileNav {
         display: flex;
-
     }
+}
+.slide-enter-active, .slide-leave-active {
+  transition: left 0.2s ease;
+}
+.slide-enter, .slide-leave-to {
+  left: -100%;
+}
+.slide-enter-to, .slide-leave {
+  left: 0;
 }
 </style>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref, onMounted, onUnmounted, nextTick } from 'vue';
 
 const darkBg = ref(false);
+const showMobileNav = ref(true);
 
 const handleScroll = () => {
     const navHeight = document.getElementById('nav').offsetHeight;
@@ -158,10 +188,21 @@ const handleScroll = () => {
     }
 };
 
+const toggleNav = async () => {
+    await nextTick(); // Ensure DOM is updated before accessing sideBar
+    const sideBar = document.getElementById('sideBar');
+    if (showMobileNav.value) {
+        sideBar.style.left = '-100%';
+    } else {
+        sideBar.style.left = '0';
+    }
+    showMobileNav.value = !showMobileNav.value;
+};
 
 
 onMounted(() => {
     window.addEventListener('scroll', handleScroll);
+    
 });
 
 onUnmounted(() => {
